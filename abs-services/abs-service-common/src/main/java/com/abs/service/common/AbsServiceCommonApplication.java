4d5c418b15026580d197b5ba06629094dd9fd0f7
xinxiamu <932852117@qq.com>
2018-01-26 19:02:52 +0800

    基础配置调整

---
+++ abs-services/abs-service-common/src/main/java/com/abs/service/common/AbsServiceCommonApplication.java
@@ -0,0 +1,12 @@
+package com.abs.service.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsServiceCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsServiceCommonApplication.class, args);
+	}
+}
