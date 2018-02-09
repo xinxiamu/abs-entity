476529624ae958911006d09bd825331234691719
xinxiamu <932852117@qq.com>
2018-02-09 20:08:53 +0800

    基础配置调整

---
+++ abs-services/abs-service-fileclient/src/main/java/com/ymu/service/fileclient/AbsServiceFileclientApplication.java
@@ -0,0 +1,12 @@
+package com.ymu.abs.absservicefileclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbsServiceFileclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbsServiceFileclientApplication.class, args);
+	}
+}
